146c241a0148d8448208449d11c1ff54508d5bf5
N C Mohit <44508697+NCMohit@users.noreply.github.com>
2019-11-27 12:37:42 +0530

    Update ChatApp.java

--- ChatApp.java
+++ ChatApp.java
@@ -3,74 +3,82 @@ import java.net.*;
 import java.util.Scanner;  
 import javax.swing.*;
 import java.awt.event.*;
-class InputStreamThread implements Runnable{ 
-	DataInputStream dis;
-	JLabel chat;
-	String opponame;
-    public InputStreamThread(DataInputStream dis,JLabel chat,String opponame){
-    	this.dis = dis;
-    	this.chat = chat;
-    	this.opponame = opponame;
-    }
-    public void run(){ 
-        try{ 
-        	while(1==1){
-        		String  str=(String)dis.readUTF();
-        		String chatstr = chat.getText();
-        		chatstr += "<br>" + opponame+": " +str;
-        		chat.setText(chatstr);
-        	}
-        } 
-        catch (Exception e){  
-            System.out.println ("Exception is caught"); 
-        } 
-    } 
-} 
-public class ChatApp extends JFrame implements ActionListener{
+import java.sql.*;
+public class ChatApplication_Mohit extends JFrame implements ActionListener{
 	JButton createserver;
 	JButton createclient;
-	JTextField enterip;
+	JTextField ip;
 	JLabel chat;
+	JLabel error;
 	String chatstr;
 	JTextField enterchat;
 	JButton send;
-	DataInputStream dis;
-	DataOutputStream dos;
+	DataInputStream is;
+	DataOutputStream os;
+	JLabel enterusernamelabel;
+	JLabel enterpasslabel;
 	JTextField enterusername;
-	JTextField enterpass;
+	JPasswordField enterpass;
 	JButton login;
+	JButton register;
 	String myname;
-	String opponame;
-	ChatApp(){
-		createserver = new JButton("Create Server");
-		createclient = new JButton("Connect to a server");
-		enterip = new JTextField("Server IP");
+	String messagegetter;
+	int loggedin = 0;
+	int usernameexists = 0;  
+	ChatApplication_Mohit(){
+
+		// GUI FOR CHAT APPLICATION
+
+		createserver = new JButton("Start Server");
+		createclient = new JButton("Connect to server");
+		ip = new JTextField("Server IP");
 		chat = new JLabel();
 		enterchat = new JTextField();
-		send = new JButton("Send");
-		enterusername = new JTextField("Username: ");
-		enterpass = new JTextField("Password: ");
+		send = new JButton("->");
+		enterusernamelabel = new JLabel("Username: ");
+		enterpasslabel = new JLabel("Password: ");
+		enterusername = new JTextField("");
+		enterpass = new JPasswordField("");
 		login = new JButton("Login");
+		register = new JButton("Register");
+		error = new JLabel("");
 
 		createserver.setBounds(50,630,150,30);
-		enterip.setBounds(430,630,100,30);
+		ip.setBounds(430,630,100,30);
 		createclient.setBounds(530,630,150,30);
 		chat.setBounds(50,50,500,500);
 		enterchat.setBounds(50,550,500,50);
 		send.setBounds(550,550,100,50);
+		enterusernamelabel.setBounds(100,250,100,50);
+		enterpasslabel.setBounds(100,350,100,50);
 		enterusername.setBounds(250,250,150,50);
 		enterpass.setBounds(250,350,150,50);
 		login.setBounds(300,450,100,50);
+		register.setBounds(300,550,100,50);
+		error.setBounds(200,600,200,50);
 
 		createserver.addActionListener(this);
 		createclient.addActionListener(this);
 		send.addActionListener(this);
 		login.addActionListener(this);
+		register.addActionListener(this);
 
-		add(createclient);add(createserver);add(enterip);add(chat);add(enterchat);add(send);add(enterusername);add(enterpass);add(login);
+		add(createclient);
+		add(createserver);
+		add(ip);
+		add(chat);
+		add(enterchat);
+		add(send);
+		add(enterusername);
+		add(enterusernamelabel);
+		add(enterpass);
+		add(enterpasslabel);
+		add(login);
+		add(register);
+		add(error);
 		enterchat.setVisible(false);
 		send.setVisible(false);
-		enterip.setVisible(false);
+		ip.setVisible(false);
 		createserver.setVisible(false);
 		createclient.setVisible(false);
 		
@@ -81,77 +89,176 @@ public class ChatApp extends JFrame implements ActionListener{
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	public void actionPerformed(ActionEvent e){		
-		if(e.getActionCommand() == "Create Server"){	
-			chatstr = "<html>"+"Waiting for connection....";
+	public void actionPerformed(ActionEvent e){
+
+		// LOGIN BUTTON CHECKS USERNAME AND PASSWORD AND MATCHES WITH VALUES IN DATABASE
+
+		if(e.getActionCommand() == "Login"){
+			try{ 
+				
+				Class.forName("com.mysql.cj.jdbc.Driver");  
+				Connection con=DriverManager.getConnection(  
+				"jdbc:mysql://localhost/userdb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
+				Statement stmt=con.createStatement();  
+				ResultSet rs=stmt.executeQuery("select * from usertable"); 
+				while(rs.next()){
+					String username = rs.getString("username");
+					String pass = rs.getString("pass");
+					// System.out.println(username+" "+pass);
+					// System.out.println(enterpass.getPassword());
+					if( enterusername.getText().equals(username) && enterpass.getText().equals(pass) ){
+						loggedin = 1;
+						break;
+					}
+				}
+				con.close();
+			}
+			catch(Exception e1){
+				error.setText("SQL server not started");
+			}
+			if(loggedin == 1){
+				ip.setVisible(true);
+				createserver.setVisible(true);
+				createclient.setVisible(true);
+				enterusername.setVisible(false);
+				enterpass.setVisible(false);
+				enterusernamelabel.setVisible(false);
+				enterpasslabel.setVisible(false);
+				login.setVisible(false);
+				register.setVisible(false);
+				error.setVisible(false);
+				myname = enterusername.getText();
+			}
+			else{
+				error.setText("Login name or Password not correct !");
+			}
+		}
+
+		// REGISTER BUTTON INSERTS USERNAME AND PASSWORD AS NEW ENTRIES IN DATABASE
+		
+		else if(e.getActionCommand() == "Register"){
+			try{
+				Class.forName("com.mysql.cj.jdbc.Driver");  
+				Connection con=DriverManager.getConnection(  
+				"jdbc:mysql://localhost/userdb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
+				Statement stmt=con.createStatement();  
+				ResultSet rs=stmt.executeQuery("select * from usertable");
+				while(rs.next()){
+					String username = rs.getString("username");
+					String pass = rs.getString("pass");
+					if((enterusername.getText() == username)){
+						error.setText("Username already taken");
+						usernameexists = 1;
+						break;
+					}
+				}
+				if(usernameexists == 0){
+					Statement stmt2=con.createStatement();  
+					stmt2.executeUpdate("INSERT INTO usertable VALUES ('"+enterusername.getText()+"','"+enterpass.getText()+"')");
+					error.setText("User succcessfully registered !");
+				}
+				con.close();
+			}
+			catch(Exception e2){
+				error.setText("SQL server not started");
+			}
+		}
+
+		//  ON PRESSING CREATE SERVER, SERVER IS STARTED ALONG WITH TEXT 
+
+		if(e.getActionCommand() == "Start Server"){	
+			chatstr = "<html>"+"User Connected !";
 			chat.setText(chatstr);
 			createclient.setEnabled(false);
 			createserver.setEnabled(false);
 			enterchat.setVisible(true);
 			send.setVisible(true);
-			createserverfunc();
+			createserverconnection();
 		}
-		else if(e.getActionCommand() == "Connect to a server"){
-			chatstr = "<html>"+"Connecting....";
+
+		// ON PRESSING CONNECT TO SERVER, SOCKETS ARE CONNECTED TO SPECIFIED IP
+
+		else if(e.getActionCommand() == "Connect to server"){
+			chatstr = "<html>"+"Connected to "+ip.getText();
 			chat.setText(chatstr);
 			createclient.setEnabled(false);
 			createserver.setEnabled(false);
 			enterchat.setVisible(true);
 			send.setVisible(true);
-			createclientfunc(enterip.getText());
+			createclientconnection(ip.getText());
 		}
-		else if(e.getActionCommand() == "Send"){
-			sendmessage(dos,enterchat.getText());
+		// THIS BUTTON IS ENABLED AFTER PRESSING EITHER CONNECT / OR CREATE SERVER, AND SENDS DATA TO THE OUTPUTSTREAM
+		else if(e.getActionCommand() == "->"){
+			send(os,enterchat.getText());
 		}
-		else if(e.getActionCommand() == "Login"){
-			enterip.setVisible(true);
-			createserver.setVisible(true);
-			createclient.setVisible(true);
-			enterusername.setVisible(false);
-			enterpass.setVisible(false);
-			login.setVisible(false);
-			myname = enterusername.getText();
-		}
+
 	}
-	public void createserverfunc(){
+	// THIS IS STARTED ON PRESSING START SERVER, THIS STARTS A INPUTSTREAM THREAD
+	public void createserverconnection(){
 		try{  
 			ServerSocket ss=new ServerSocket(6666);  
-			Socket s=ss.accept();//establishes connection   
-			dis=new DataInputStream(s.getInputStream());
-			dos=new DataOutputStream(s.getOutputStream());
-			opponame =(String)dis.readUTF();
-			dos.writeUTF(myname);
-			Thread myinobject = new Thread(new InputStreamThread(dis,chat,opponame)); 
-            myinobject.start(); 
+			Socket s=ss.accept();  
+			is=new DataInputStream(s.getInputStream());
+			os=new DataOutputStream(s.getOutputStream());
+			messagegetter =(String)is.readUTF();
+			os.writeUTF(myname);
+			Thread inputstreamthreadobject = new Thread(new ISThread(is,messagegetter,chat)); 
+            inputstreamthreadobject.start(); 
 		}catch(Exception e){System.out.println(e);} 		
 	}
-	public void createclientfunc(String ip){
+	// THIS IS STARTED ON PRESSING CONNECT, THIS STARTS A INPUTSTREAM THREAD
+	public void createclientconnection(String ip){
 		try{
 			Socket s=new Socket(ip,6666);  
-			dos=new DataOutputStream(s.getOutputStream());
-			dis=new DataInputStream(s.getInputStream());
-			dos.writeUTF(myname);  
-			opponame =(String)dis.readUTF();
-			Thread myinobject = new Thread(new InputStreamThread(dis,chat,opponame)); 
-            myinobject.start();   
+			os=new DataOutputStream(s.getOutputStream());
+			is=new DataInputStream(s.getInputStream());
+			os.writeUTF(myname);  
+			messagegetter =(String)is.readUTF();
+			Thread inputstreamthreadobject = new Thread(new ISThread(is,messagegetter,chat)); 
+            inputstreamthreadobject.start();   
 		}catch(Exception e){System.out.println(e);}  		
 	}
-	public void sendmessage(DataOutputStream dos,String msg){
+	// THIS IS EXECUTED ON PRESSING THE BUTTON SEND, SENDS MESSAGE IN BOX TO OUTPUT STREAM AND LOGS THE INFORMATION
+	public void send(DataOutputStream os,String msg){
 		try{
-			dos.writeUTF(msg);
+			os.writeUTF(msg);
 			String chatstr = chat.getText();
-			chatstr += "<br>" + myname+": " + msg;
+			chatstr += "<br>" + myname+"-> " + msg;
 			chat.setText(chatstr);
 			String path = System.getProperty("user.dir");
-			FileWriter fw=new FileWriter(path+"\\log.html");
+			FileWriter fw=new FileWriter(path+"\\chathistory.html");
 			fw.write(chat.getText());
 			fw.close();
 		}
 		catch(Exception e){
-			System.out.println("Error!");
+			System.out.println("Error sending message!");
 		}
 	}
 	public static void main(String[] args) {
-		new ChatApp();
+		new ChatApplication_Mohit();
 	}
 }
+// THIS THREAD IS USED TO KEEP ON RUNNING INPUTSTREAM WITHOUT EFFECTING OTHER FUNCTIONALITIES OF CHAT APPLICATION
+class ISThread implements Runnable{ 
+	DataInputStream is;
+	JLabel chat;
+	String messagegetter;
+    public ISThread(DataInputStream is,String messagegetter,JLabel chat){
+    	this.is = is;
+    	this.chat = chat;
+    	this.messagegetter = messagegetter;
+    }
+    public void run(){ 
+        try{ 
+        	while(1==1){
+        		String  str=(String)is.readUTF();
+        		String chatstr = chat.getText();
+        		chatstr += "<br>" + messagegetter+"-> " +str;
+        		chat.setText(chatstr);
+        	}
+        } 
+        catch (Exception e){  
+            System.out.println ("Server has been closed"); 
+        } 
+    } 
+} 
